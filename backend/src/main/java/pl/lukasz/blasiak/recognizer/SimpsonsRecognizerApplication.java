bdaf20cdb3596e8a149f467d370295effb839e8e
lukasz <lukasz.blasiak@wp.pl>
2019-11-11 17:09:06 +0100

    Create spring boot application

---
+++ backend/src/main/java/pl/lukasz/blasiak/recognizer/SimpsonsRecognizerApplication.java
@@ -0,0 +1,11 @@
+package pl.lukasz.blasiak.recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpsonsRecognizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpsonsRecognizerApplication.class, args);
+    }
+}
